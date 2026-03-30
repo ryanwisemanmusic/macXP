293d2b629299f5ffff35354e170916c5922f131e
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-03-30 01:23:04 -0700

    /window/entrypoint.c
    Tests to see if a valid Win32 to SDL3 window can be created, which it can

--- ui/window/entrypoint.c
+++ ui/window/entrypoint.c
@@ -4,6 +4,8 @@
 #include <SDL3_image/SDL_image.h>
 #include <SDL3_mixer/SDL_mixer.h>
 
+#include "win32functions.h"
+
 static void ui_window_header_probe(void) {
 	SDL_Window *window = NULL;
 	SDL_Surface *surface = NULL;
@@ -15,7 +17,24 @@ static void ui_window_header_probe(void) {
 }
 
 __attribute__((constructor)) static void ui_window_entrypoint_bootstrap(void) {
+	int gate_code = win32functions_gate_windowing();
+
+	if (gate_code != WIN32FUNC_GATE_OK) {
+		fprintf(stderr,
+			"[ui/window] Win32 gate failed: %s. SDL path blocked.\n",
+			win32functions_gate_error_string(gate_code));
+		return;
+	}
+
+	gate_code = win32functions_smoke_visible_window(900);
+	if (gate_code != WIN32FUNC_GATE_OK) {
+		fprintf(stderr,
+			"[ui/window] Win32 visible smoke failed: %s. SDL path blocked.\n",
+			win32functions_gate_error_string(gate_code));
+		return;
+	}
+
 	ui_window_header_probe();
-	fprintf(stderr, "[ui/window] SDL headers resolved (SDL3, SDL_image, SDL_mixer).\n");
+	fprintf(stderr, "[ui/window] Win32 gate passed. Visible create/destroy smoke passed. SDL headers resolved (SDL3, SDL_image, SDL_mixer).\n");
 }
 
